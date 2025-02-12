fd569104017d485e9ccbb12550cc3430e6ba2706
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-02-12 18:16:38 +0100

    Step 1 Implemented

--- parser.py
+++ parser.py
@@ -1,8 +1,13 @@
-
-
+def check_object_syntax(data:str):
+    if not data:
+        raise Exception("Empty Object")
+    
+    if data[0] != '{' or data[-1] != '}':
+        raise Exception("Syntax is incorrect")
 
 def main():
-    print("Hello World!")
+    data = open("./tests/step1/valid.json", "r").read()
+    check_object_syntax(data)
 
 
 
